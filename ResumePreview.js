98faf69794716164222efa198385b9e222e8d9d5
varsha737 <rituvarsha7370@gmail.com>
2025-05-09 03:11:28 +0530

    Add files via upload

---
+++ ResumePreview.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ResumePreview = ({ formData }) => {
+  return (
+    <div className="resume-preview">
+      <h2>{formData.name}</h2>
+      <p>{formData.email}</p>
+      <p>{formData.phone}</p>
+      <p>{formData.experience}</p>
+      <p>{formData.skills}</p>
+    </div>
+  );
+};
+
+export default ResumePreview;
